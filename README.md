6a14f1b565823b992f0b2787a9659983b3e544f3
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-07-21 14:27:15 -0400

    Find <div> with class name

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Web Scraping w/ Beautiful Soup
 
+### HTML Code
+Link to Expired Job Posting: https://www.indeed.com/viewjob?jk=0be852a756d74baf&hl=en
+
+
+
 ### Resources
 * [RealPython - Beautiful Soup Web Scraper Python](https://realpython.com/beautiful-soup-web-scraper-python/)
--- index.py
+++ index.py
@@ -1,3 +1,14 @@
 import requests 
 from bs4 import BeautifulSoup
 
+URL = "https://www.indeed.com/viewjob?jk=0be852a756d74baf&hl=en"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(role="alert")
+
+print(results.prettify())
+
+job_elements = results.find_all("div", class_="icl-Alert-headline")
+
